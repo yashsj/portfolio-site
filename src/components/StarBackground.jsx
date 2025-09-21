10c102c3082a51c1a986ea1bd494a5ee97198472
Suyash <Suyashsj@hotmail.com>
2025-09-21 17:44:13 -0500

    Smoother animations

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -32,21 +32,22 @@ export const StarBackground = () => {
         setStars(newStars); // FIX 2: Set state only once, after the loop
     };
 
-    const generateMeteors = () => {
-        const numberOfMeteors = 4
-        const newMeteors = [];
-        for (let i = 0; i < numberOfMeteors; i++) {
-            newMeteors.push({
-                id: i,
-                size: Math.random() * 2 + 1,
-                x: Math.random() * 100,
-                y: Math.random() * 20,
-                delay:Math.random()*15,
-                animationDuration: Math.random() * 3 + 3,
-            });
-        }
-        setMeteors(newMeteors); // FIX 2: Set state only once, after the loop
-    };
+   const generateMeteors = () => {
+    const numberOfMeteors = 4;
+    const newMeteors = [];
+    for (let i = 0; i < numberOfMeteors; i++) {
+        newMeteors.push({
+            id: i,
+            size: Math.random() * 2 + 1,
+            x: Math.random() * 100,
+            y: Math.random() * 20,
+            delay: 0, // <--- Change here for instant animation
+            animationDuration: Math.random() * 3 + 3,
+        });
+    }
+    setMeteors(newMeteors);
+};
+
 
     return (
         <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
@@ -66,19 +67,18 @@ export const StarBackground = () => {
             ))}
 
             {meteors.map((meteor) => (
-                <div
-                    key={meteor.id}
-                    className="meteor animate-meteor" // Make sure you have a .star class in your CSS!
-                    style={{
-                        width: meteor.size *50 +"px",
-                        height: meteor.size + "px",
-                        left: meteor.x + "%", // FIX 3: Use the correct x coordinate
-                        top: meteor.y + "%",
-                        animationDelay: meteor.delay + "s",
-                        animationDuration: meteor.animationDuration + "s",
-                    }}
-                />
-            ))}
+    <div
+        key={meteor.id}
+        className="meteor"
+        style={{
+            width: meteor.size * 50 + "px",
+            height: meteor.size + "px",
+            left: meteor.x + "%",
+            top: meteor.y + "%",
+            animation: `meteor ${meteor.animationDuration}s linear ${meteor.delay}s infinite`,
+        }}
+    />
+))}
         </div>
 
     );
